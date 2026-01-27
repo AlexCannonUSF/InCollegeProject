3e58277f302ca619055ce62b4147240ae40b5918
RaulCastillo547 <morales7258@gmail.com>
2026-01-27 02:18:07 +0000

    Updated Test File

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 for input_file in tests/inputs/*.txt; do
-    if [$input_file != "tests/test.sh"]; then
+    if [$input_file == "tests/test.sh"]; then
         continue
     fi
 
+    echo "" > ./data/accounts.dat
     FILENAME=$(echo $input_file | cut -d '/' -f 3)
     
     ACTUAL_OUTPUT_FILE_NAME=("tests/actual_outputs/$FILENAME")
