b81cf7800ec993c8f2b4db391394584814cd2b9f
RaulCastillo547 <morales7258@gmail.com>
2026-01-27 03:49:20 +0000

    Add MaxAccount Tests

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,7 @@ for input_file in tests/inputs/*.txt; do
         continue
     fi
 
-    echo "" > ./data/accounts.dat
+    > ./data/accounts.dat
     FILENAME=$(echo $input_file | cut -d '/' -f 3)
     
     ACTUAL_OUTPUT_FILE_NAME=("tests/actual_outputs/$FILENAME")
