2f21a9dbab8e1d165c4539e8c4397d91ab4c2884
Harvey Bui <huyvijamco@gmail.com>
2026-01-31 20:18:43 -0500

    Fix: data ordering, data naming and function ending

--- src/PersonalProfile.cob
+++ src/PersonalProfile.cob
@@ -5,31 +5,35 @@ PROGRAM-ID. PersonalProfile.
 ENVIRONMENT DIVISION.
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-01 WorkCount         PIC 9(2) VALUE 0.
-01 EduCount          PIC 9(2) VALUE 0.
+77 I                 PIC 9(2) VALUE 0.
+77 WorkCount         PIC 9(2) VALUE 0.
+77 EduCount          PIC 9(2) VALUE 0.
 01 UserProfile.
    05 Name            PIC X(50).
-   05 University       PIC X(50).
+   05 UserUniversity  PIC X(50).
    05 Major           PIC X(50).
    05 GraduationYear  PIC 9(4).
    05 AboutMe         PIC X(200).
-   05 WorkExperience   OCCURS 0 TO 3 TIMES DEPENDING ON WorkCount.
+   05 WorkExperience   OCCURS 3 TIMES.
       10 JobTitle      PIC X(50).
-      10 Company   PIC X(50).
-      10 Dates   PIC X(30).
-      10 Description PIC X(200).
-   05 EducationHistory OCCURS 0 TO 3 TIMES DEPENDING ON EduCount.
+      10 Company       PIC X(50).
+      10 Dates         PIC X(30).
+      10 Description   PIC X(200).
+   05 EducationHistory OCCURS 3 TIMES.
       10 Degree        PIC X(50).
-      10 University PIC X(50).
-      10 Years      PIC X(30).
-77 I PIC 9(2) VALUE 0.
-PROCEDURE DIVISION.
+      10 EduUniversity PIC X(50).
+      10 Years         PIC X(30).
+LINKAGE SECTION.
+01 LK-USER-NAME PIC X(20).
+
+PROCEDURE DIVISION USING LK-USER-NAME.
 MAIN.
     DISPLAY 'Welcome to Your Personal Profile Page'.
-    DISPLAY 'Name: ' FUNCTION TRIM(Name).                                
-    DISPLAY 'University: ' FUNCTION TRIM(University).
-    DISPLAY 'Major: ' FUNCTION (Major).
-    DISPLAY 'Graduation Year: ' FUNCTION TRIM(GraduationYear).
+    DISPLAY 'Viewing Profile for: ' FUNCTION TRIM(LK-USER-NAME).
+    DISPLAY 'Name: ' FUNCTION TRIM(Name).
+    DISPLAY 'University: ' FUNCTION TRIM(UserUniversity).
+    DISPLAY 'Major: ' FUNCTION TRIM(Major).
+    DISPLAY 'Graduation Year: ' GraduationYear.
     DISPLAY 'About Me: ' FUNCTION TRIM(AboutMe).
     PERFORM DISPLAY-WORK-EXPERIENCE.
     PERFORM DISPLAY-EDUCATION.
@@ -41,11 +45,11 @@ DISPLAY-WORK-EXPERIENCE.
         DISPLAY 'No work experience.'
     ELSE
         PERFORM VARYING I FROM 1 BY 1 UNTIL I > WorkCount
-            DISPLAY 'Title: ' FUNCTION TRIM(WorkExperience(I).JobTitle).
-            DISPLAY 'Company: ' FUNCTION TRIM(WorkExperience(I).Company).
-            DISPLAY 'Dates: ' FUNCTION TRIM(WorkExperience(I).Dates).
-            DISPLAY 'Description: ' FUNCTION TRIM(WorkExperience(I).Description).
-        END-PERFORM.
+            DISPLAY 'Title: ' FUNCTION TRIM(WorkExperience(I).JobTitle)
+            DISPLAY 'Company: ' FUNCTION TRIM(WorkExperience(I).Company)
+            DISPLAY 'Dates: ' FUNCTION TRIM(WorkExperience(I).Dates)
+            DISPLAY 'Description: ' FUNCTION TRIM(WorkExperience(I).Description)
+        END-PERFORM
     END-IF.
 
 DISPLAY-EDUCATION.
@@ -54,8 +58,10 @@ DISPLAY-EDUCATION.
         DISPLAY 'No education history.'
     ELSE
         PERFORM VARYING I FROM 1 BY 1 UNTIL I > EduCount
-            DISPLAY 'Degree: ' FUNCTION TRIM(EducationHistory(I).Degree).
-            DISPLAY 'University: ' FUNCTION TRIM(EducationHistory(I).University).
-            DISPLAY 'Years: ' FUNCTION TRIM(EducationHistory(I).Years).
-        END-PERFORM.
+            DISPLAY 'Degree: ' FUNCTION TRIM(EducationHistory(I).Degree)
+            DISPLAY 'University: ' FUNCTION TRIM(EducationHistory(I).EduUniversity)
+            DISPLAY 'Years: ' FUNCTION TRIM(EducationHistory(I).Years)
+        END-PERFORM
     END-IF.
+
+END PROGRAM PersonalProfile.
