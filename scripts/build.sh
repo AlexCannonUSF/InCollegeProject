b4e4efacd5fe0cb768475526c8486d8f4b287a02
Harvey Bui <huyvijamco@gmail.com>
2026-02-02 18:47:02 -0500

    Fix: build path

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,14 +4,14 @@ set -e
 mkdir -p bin
 
 cobc -x -free -o bin/InCollege \
-  ../src/InCollege.cob \
-  ../src/CreateAccount.cob \
-  ../src/LogIn.cob \
-  ../src/DataStore.cob \
-  ../src/HomePage.cob \
-  ../src/SkillMenu.cob \
-  ../src/PersonalProfile.cob \
-  ../src/ProfileCreate.cob \
-  ../src/ProfileEdit.cob
+  src/InCollege.cob \
+  src/CreateAccount.cob \
+  src/LogIn.cob \
+  src/DataStore.cob \
+  src/HomePage.cob \
+  src/SkillMenu.cob \
+  src/PersonalProfile.cob \
+  src/ProfileCreate.cob \
+  src/ProfileEdit.cob
 
 echo "Build complete: bin/InCollege"
