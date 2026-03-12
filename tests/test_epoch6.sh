0e64bbd672aa6e7388248bd9f0ac4d024302c877
Morales7258@gmail.com <morales7258@gmail.com>
2026-03-12 17:14:20 +0000

    Added Persistence Test Checks

--- tests/test_epoch6.sh
+++ tests/test_epoch6.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/usr/bin/bash
+set -uo pipefail
 
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 EPOCH="epoch_6"
 IN_DIR="$ROOT/tests/$EPOCH/inputs"
 CORRECT_OUT_DIR="$ROOT/tests/$EPOCH/expected_outputs"
-OUT_DIR="$ROOT/tests/$EPOCH/actual_outputs"
+ACTUAL_OUT_DIR="$ROOT/tests/$EPOCH/actual_outputs"
 
-mkdir -p "$OUT_DIR"
+mkdir -p "$ACTUAL_OUT_DIR"
 
 for input in "$IN_DIR"/*.txt; do
   name="$(basename "$input")"
@@ -22,29 +22,34 @@ for input in "$IN_DIR"/*.txt; do
   : > "$ROOT/data/PendingRequests.dat"
   : > "$ROOT/data/JobPostings.dat"
 
-  (cd "$ROOT" && "$ROOT/bin/InCollege" < "$input") > "$OUT_DIR/$name"
+  (cd "$ROOT" && "$ROOT/bin/InCollege" < "$input") > "$ACTUAL_OUT_DIR/$name"
 
-  cat "$ROOT/data/JobPostings.dat" > "$OUT_DIR/${name::-4}-Persistence.txt"
+  if [ -f "$CORRECT_OUT_DIR/${name::-4}-Persistence.txt" ]; then
+    cat "$ROOT/data/JobPostings.dat" > "$ACTUAL_OUT_DIR/${name::-4}-Persistence.txt"
+  fi
 
   # ensure output ends with newline (prevents "\ No newline at end of file" diffs)
-  # printf '\n' >> "$OUT_DIR/$name"
 
-  DIFF=$(diff "$OUT_DIR/$name" "$CORRECT_OUT_DIR/$name" | cat)
-  if [$DIFF -eq ""]; then
+  echo "Running Output Tests"
+  DIFF=$(diff "$ACTUAL_OUT_DIR/$name" "$CORRECT_OUT_DIR/$name")
+  if [ ! "$DIFF" ]; then
     echo "Pass Output Tests"
   else
     echo "Failed Output Tests"
     echo "$DIFF"
   fi
 
-  DIFF=$(diff "$OUT_DIR/${name::-4}-Persistence.txt" "$CORRECT_OUT_DIR/${name::-4}-Persistence.txt" | cat)
-  if [$DIFF -eq ""]; then
-    echo "Pass Persistence Tests"
-  else
-    echo "Failed Persistence Tests"
-    echo "$DIFF"
+  if [ -f "$CORRECT_OUT_DIR/${name::-4}-Persistence.txt" ]; then
+    echo "Running Persistence Tests"
+    DIFF=$(diff "$ACTUAL_OUT_DIR/${name::-4}-Persistence.txt" "$CORRECT_OUT_DIR/${name::-4}-Persistence.txt");
+    if [ ! "$DIFF" ]; then
+      echo "Pass Persistence Tests"
+    else
+      echo "Failed Persistence Tests"
+      echo "$DIFF"
+    fi
   fi
     
 done
 
-echo "Done. Actual outputs in: $OUT_DIR"
+echo "Done. Actual outputs in: $ACTUAL_OUT_DIR"
