20e26243febd553f2881afefb0fc39d9b6d755ef
Morales7258@gmail.com <morales7258@gmail.com>
2026-04-05 22:46:22 +0000

    Added ability to read messages

--- src/ViewMessages.cob
+++ src/ViewMessages.cob
@@ -34,21 +34,31 @@ MAIN.
 
        OPEN INPUT MESSAGE-FILE
        IF WS-MESSAGE-FILE-STATUS = "35"
+           DISPLAY "--- Your Messages ---"
            DISPLAY "You have no messages at this time."
            DISPLAY "---------------------"
            GOBACK
        END-IF
 
+       DISPLAY "--- Your Messages ---"
        PERFORM UNTIL WS-EOF-MSG = 'Y'
            READ MESSAGE-FILE
                AT END
                    MOVE 'Y' TO WS-EOF-MSG
                NOT AT END
+                   IF WS-MSG-COUNT > 0
+                       DISPLAY ""
+                   END-IF
+
                    IF FUNCTION TRIM(MESSAGE-RECIPIENT) = FUNCTION TRIM(LNK-USER-NAME)
                        ADD 1 TO WS-MSG-COUNT
                        *> Message display format goes here
                        *> MESSAGE-SENDER
                        *> MESSAGE-CONTENT, MESSAGE-TIMESTAMP
+                       DISPLAY "From: " FUNCTION TRIM(MESSAGE-SENDER)
+                       DISPLAY "Message: " FUNCTION TRIM(MESSAGE-CONTENT)
+                       DISPLAY "Sent: " FUNCTION TRIM(MESSAGE-TIMESTAMP)
+                       DISPLAY "---" WITH NO ADVANCING
                    END-IF
            END-READ
        END-PERFORM
@@ -58,6 +68,8 @@ MAIN.
        IF WS-MSG-COUNT = 0
            DISPLAY "You have no messages at this time."
            DISPLAY "---------------------"
+       ELSE
+           DISPLAY "------------------"
        END-IF
 
        GOBACK.
